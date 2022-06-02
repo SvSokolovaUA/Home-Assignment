f1e9ec8a6df844aa9e58938415243de650615c72
Svitlana <svitlana.sokolova@gmail.com>
2022-06-02 10:04:50 +0200

    commit

--- HomeAsmnts/HA_20.js
+++ HomeAsmnts/HA_20.js
@@ -1,7 +1,9 @@
 function getFizzBuzz(n) {
-    if (n % 3 == 0) {
+    if (n % 3 == 0 && n % 5 != 0) {
         return "Fizz";
-    } else if (n % 5 == 0) {
+    } else if (n % 5 == 0 &&  n % 3 !=0) {
         return "Buzz";
+    } else if (n % 5 == 0 && n % 3 == 0) {
+        return "FizzBuzz";
    }
 }
