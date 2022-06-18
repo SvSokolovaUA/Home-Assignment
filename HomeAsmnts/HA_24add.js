65cd8f9bdd9637a7b891d5ca506e4d44785c6694
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-06-18 15:29:47 +0300

    last tests

--- HomeAsmnts/HA_24add.js
+++ HomeAsmnts/HA_24add.js
@@ -3,25 +3,26 @@ function tellToBob(sent) {
     let newSent = sent.substr(0, sent.length - 1);
     let lastLetter = sent[sent.length - 1];
     
-    if (getIfUpper(newSent) && lastLetter == "?") {
-        return "Успокойся, я знаю, что делаю!";
-    }
-    
-    if (!getIfUpper(newSent) && lastLetter == "?") {
-        return "Конечно";
-    } 
-
-    if (getIfUpper(sent) == true) {
+    if (sent.length == 0) {
+        return "Хорошо. Пусть так и будет!";
+    } else if (lastLetter == "?") {
+        if (!getIfUpper(newSent)) {
+            return "Конечно";
+        } else {return "Успокойся, я знаю, что делаю!"};
+    } else if (getIfUpper(sent)) {
         return "Вау, расслабься!";
+    } else {
+        return "Всё равно";
     }
-    
 }
 
 function getIfUpper (sent) {
+    
     for (let i=0; i<sent.length; i+=1) {
         let letter = sent[i];
         if (letter == letter.toLowerCase()) {
             return false;
-        } else {return true};
+        }
     }
+    return true;
 }
--- spec/24spec_ha.js
+++ spec/24spec_ha.js
@@ -17,4 +17,16 @@ describe("small talk with Bob", function () {
         expect(result).to.equal("Успокойся, я знаю, что делаю!");
     });
 
+    it("receive'Ok, let it be' if say nothing", function () {
+        const result = tellToBob("");
+
+        expect(result).to.equal("Хорошо. Пусть так и будет!");
+    });
+
+    it("receive'No matter' if say anithing else", function () {
+        const result = tellToBob("Привет");
+
+        expect(result).to.equal("Всё равно");
+    });
+
 })
