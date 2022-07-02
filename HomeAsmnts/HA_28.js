b4a1b98977885ed59e94d17cf15363d3b1391cfd
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-07-02 18:01:39 +0300

    design in js doc

---
+++ HomeAsmnts/HA_28.js
@@ -0,0 +1,41 @@
+'use strict';
+let val;
+
+const div = document.querySelector("div");
+val = div.style.color = "rgb(7, 1, 53)";
+
+const blockhead1 = document.querySelector("#blockhead1");
+val = blockhead1.style.color = "rgb(7, 1, 53)";
+val = blockhead1.style.fontWeight = "bold";
+
+const blockhead2 = document.querySelector("#blockhead2");
+val = blockhead2.style.color = "rgb(7, 1, 53)";
+val = blockhead2.style.fontWeight = "bold";
+
+const blockhead3 = document.querySelector("#blockhead3");
+val = blockhead3.style.color = "rgb(7, 1, 53)";
+val = blockhead3.style.fontWeight = "bold";
+
+const blockbody1 = querySelector("#blockbody1");
+val = blockbody1.style.color = "rgb(43, 41, 53)";
+
+const blockbody2 = querySelector(".blockbody");
+val = blockbody2.style.color = "rgb(43, 41, 53)";
+
+const blockbody3 = getElementById("blockbody3");
+val = blockbody3.style.color = "rgb(43, 41, 53)";
+
+const blockbody4 = getElementById("blockbody4");
+val = blockbody4.style.color = "rgb(43, 41, 53)";
+
+const blockbody5 = getElementById("blockbody5");
+val = blockbody5.style.color = "rgb(43, 41, 53)";
+
+const exampleRev = getElementById("exampleRev");
+val = exampleRev.style.color = "rgb(43, 41, 53)";
+val = exampleRev.style.fontWeight = "bold";
+val = exampleRev.style.fontStyle = "italic";
+
+const example = querySelector(".example");
+val = example.style.fontWeight = "bold";
+val = example.style.fontStyle = "italic";
