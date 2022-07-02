623c8a6010d40eb17cddec15e78ea7bfe8852ab3
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-07-02 18:30:21 +0300

    new

--- HomeAsmnts/HA_28.js
+++ HomeAsmnts/HA_28.js
@@ -1,41 +1,40 @@
 'use strict';
-let val;
 
 const div = document.querySelector("div");
-val = div.style.color = "rgb(7, 1, 53)";
+div.style.color = "rgb(7, 1, 53)";
 
 const blockhead1 = document.querySelector("#blockhead1");
-val = blockhead1.style.color = "rgb(7, 1, 53)";
-val = blockhead1.style.fontWeight = "bold";
+blockhead1.style.color = "rgb(7, 1, 53)";
+blockhead1.style.fontWeight = "bold";
 
 const blockhead2 = document.querySelector("#blockhead2");
-val = blockhead2.style.color = "rgb(7, 1, 53)";
-val = blockhead2.style.fontWeight = "bold";
+blockhead2.style.color = "rgb(7, 1, 53)";
+blockhead2.style.fontWeight = "bold";
 
 const blockhead3 = document.querySelector("#blockhead3");
-val = blockhead3.style.color = "rgb(7, 1, 53)";
-val = blockhead3.style.fontWeight = "bold";
+blockhead3.style.color = "rgb(7, 1, 53)";
+blockhead3.style.fontWeight = "bold";
 
-const blockbody1 = querySelector("#blockbody1");
-val = blockbody1.style.color = "rgb(43, 41, 53)";
+const blockbody1 = document.querySelector("#blockbody1");
+blockbody1.style.color = "rgb(43, 41, 53)";
 
-const blockbody2 = querySelector(".blockbody");
-val = blockbody2.style.color = "rgb(43, 41, 53)";
+const blockbody2 = document.querySelector(".blockbody");
+blockbody2.style.color = "rgb(43, 41, 53)";
 
-const blockbody3 = getElementById("blockbody3");
-val = blockbody3.style.color = "rgb(43, 41, 53)";
+const blockbody3 = document.getElementById("blockbody3");
+blockbody3.style.color = "rgb(43, 41, 53)";
 
-const blockbody4 = getElementById("blockbody4");
-val = blockbody4.style.color = "rgb(43, 41, 53)";
+const blockbody4 = document.getElementById("blockbody4");
+blockbody4.style.color = "rgb(43, 41, 53)";
 
-const blockbody5 = getElementById("blockbody5");
-val = blockbody5.style.color = "rgb(43, 41, 53)";
+const blockbody5 = document.getElementById("blockbody5");
+blockbody5.style.color = "rgb(43, 41, 53)";
 
-const exampleRev = getElementById("exampleRev");
-val = exampleRev.style.color = "rgb(43, 41, 53)";
-val = exampleRev.style.fontWeight = "bold";
-val = exampleRev.style.fontStyle = "italic";
+const exampleRev = document.getElementById("exampleRev");
+exampleRev.style.color = "rgb(43, 41, 53)";
+exampleRev.style.fontWeight = "bold";
+exampleRev.style.fontStyle = "italic";
 
-const example = querySelector(".example");
-val = example.style.fontWeight = "bold";
-val = example.style.fontStyle = "italic";
+const example = document.querySelector(".example");
+example.style.fontWeight = "bold";
+example.style.fontStyle = "italic";
