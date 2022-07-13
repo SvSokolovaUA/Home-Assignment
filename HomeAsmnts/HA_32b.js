e9ca62a79bc6f29832f2b171b4cb465b27f3c442
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-07-13 17:50:49 +0300

    sorted most expansive good

--- HomeAsmnts/HA_32b.js
+++ HomeAsmnts/HA_32b.js
@@ -7,7 +7,7 @@
     "white bread": 1.3,
     "sour cream": 2.5,
     "beer": 1.15,
-    cheese: 2.5,
+    cheese: 3.5,
   }
   
   const clients = [
@@ -35,21 +35,38 @@
   // Используя датасет с H.A. 31
   // 1. Сортированный список клиентов с информацией об общей стоимости корзины для каждого, 
   // сортировка по стоимости корзины, в убывающем порядке
-  
 
-const clientsBasket = clients.map(c => 
-    ({name: c, 
-    basketCost: c.goods.reduce((prev, cur) => prices[prev] + prices[cur], 0)}));
-
- 
-    console.log(clientsBasket);
-
+    const clientsBasket = clients.map(c => 
+        ({name: c, basketCost: c.goods.reduce((prev, cur) => prev + prices[cur], 0)}))
+        .sort((a, b) => b.basketCost - a.basketCost);
+     
+        console.log(clientsBasket);
+    
 
   // 2. Сортированный список только тех клиентов, стоимость корины у которых превосходит 3 евро, 
   // с информацией об общей стоимости корзины для каждого, сортировка по стоимости корзины, 
   // в убывающем порядке
-
+ 
+  const clientsBasketMoreThree = clients.map(c => 
+    ({name: c, basketCost: c.goods.reduce((prev, cur) => prev + prices[cur], 0)}))
+    .filter(c => c.basketCost > 3)
+    .sort((a, b) => b.basketCost - a.basketCost);
+ 
+    console.log(clientsBasketMoreThree);
 
   // 3. Сортированный список только тех клиентов, стоимость корины у которых превосходит 3 евро, 
   // с информацией об общей стоимости корзины для каждого, сортировка по самой дорогой покупке, 
   // в убывающем порядке
+
+prices.nothing = 0;
+
+    const clientsExpensiveGoods = clients.map(c => 
+        ({name: c, basketCost: c.goods.reduce((prev, cur) => prev + prices[cur], 0)}))
+        .filter(c => c.basketCost > 3)
+        .map(c => ({name: c, basketCost: c, 
+            MostExpensive: c.goods.reduce((prev, cur) => 
+            prices[prev] < prices[cur] ? cur: prev, prices.nothing)
+        }));
+
+    
+        console.log(clientsExpensiveGoods);
