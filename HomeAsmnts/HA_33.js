6cc42f8283a1fba4e71ff3155669343ace9268f5
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-09-15 15:01:13 +0300

    HA_33_loop

--- HomeAsmnts/HA_33.js
+++ HomeAsmnts/HA_33.js
@@ -4,13 +4,39 @@ const val = document.querySelector("body");
 checkChilds(val, 1);
 
 function checkChilds(elem, parentLevel) {
-let arr = Array.from(elem.children);
-for (let i =0; i < arr.length; i+=1) {
-    let el = arr[i];
-    console.log(parentLevel+1, el.nodeType, el.nodeName, el.className);
-    if(el.hasChildNodes) {
-        checkChilds(el, parentLevel+1);
-    }
-}
-}
 
+    let arr = Array.from(elem.children);
+
+    let i =0;
+    let el = arr[i];
+    
+    for (; ; ) {
+        console.log(parentLevel+1, el.nodeType, el.nodeName, el.className);
+
+        if (el.firstElementChild !== null) {
+            
+            el = el.firstElementChild;
+            parentLevel+=1;
+            
+        } else if (el.nextElementSibling !== null) {
+            el = el.nextElementSibling;
+            
+        } else if (el.nextElementSibling === null && i < arr.length) { 
+           el = arr[i+=1];
+        parentLevel = 1;
+        }
+    }
+    }
+
+
+
+// function checkChilds(elem, parentLevel) {
+// let arr = Array.from(elem.children);
+// for (let i =0; i < arr.length; i+=1) {
+//     let el = arr[i];
+//     console.log(parentLevel+1, el.nodeType, el.nodeName, el.className);
+//     if(el.hasChildNodes) {
+//         checkChilds(el, parentLevel+1);
+//     }
+//     }
+// }
