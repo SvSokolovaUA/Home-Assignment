54df7011a663c94ae9760a2cacd0ae0e28be7223
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-09-15 17:56:17 +0300

    HA_33_without_array

--- HomeAsmnts/HA_33.js
+++ HomeAsmnts/HA_33.js
@@ -1,32 +1,64 @@
 'use strict';
+
 const val = document.querySelector("body");
 
 checkChilds(val, 1);
 
-function checkChilds(elem, parentLevel) {
+function checkChilds(el, parentLevel) {
 
-    let arr = Array.from(elem.children);
-
-    let i =0;
-    let el = arr[i];
-    
     for (; ; ) {
-        console.log(parentLevel+1, el.nodeType, el.nodeName, el.className);
-
+        
         if (el.firstElementChild !== null) {
-            
             el = el.firstElementChild;
             parentLevel+=1;
-            
+            console.log(parentLevel, el.nodeType, el.nodeName, el.className);
+
         } else if (el.nextElementSibling !== null) {
             el = el.nextElementSibling;
+            console.log(parentLevel, el.nodeType, el.nodeName, el.className);
             
-        } else if (el.nextElementSibling === null && i < arr.length) { 
-           el = arr[i+=1];
-        parentLevel = 1;
+        } else if (el.firstElementChild === null && el.nextElementSibling === null && parentLevel > 1) {
+            for (let i = 1; i < parentLevel-1; i+=1) {
+                el = el.parentNode;
+            }
+            if (el.nextElementSibling !== null) {
+            el = el.nextElementSibling;
+            parentLevel = 2;
+            console.log(parentLevel, el.nodeType, el.nodeName, el.className);
+            }
+        } else {
+            break;
         }
     }
     }
+
+
+// function checkChilds(elem, parentLevel) {
+
+//     let arr = Array.from(elem.children);
+
+//     let i =0;
+//     let el = arr[i];
+    
+//     for (; ; ) {
+//         console.log(parentLevel+1, el.nodeType, el.nodeName, el.className);
+
+//         if (el.firstElementChild !== null) {
+            
+//             el = el.firstElementChild;
+//             parentLevel+=1;
+            
+//         } else if (el.nextElementSibling !== null) {
+//             el = el.nextElementSibling;
+            
+//         } else if (el.nextElementSibling === null && i < arr.length) { 
+//            el = arr[i+=1];
+//            parentLevel = 1;
+//         } else if (i >= arr.length) {
+//             break;
+//         }
+//     }
+//     }
 
 
 
