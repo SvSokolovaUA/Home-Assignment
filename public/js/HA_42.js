f528cb2830f36c5fb99689afcd5942b80334d9fa
SvSokolovaUA <svitlana.sokolova@gmail.com>
2022-12-01 09:17:54 +0000

    HA_42_dont_work_correctly_when_page_reloaded

---
+++ public/js/HA_42.js
@@ -0,0 +1,190 @@
+'use strict';
+
+const post = (url, data) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+const tasks = document.getElementById("task-list");
+const form = document.getElementById("todo-form");
+const taskInput = form.querySelector("input");
+const clearTasksButton = document.getElementsByClassName("clear-tasks button")[0];
+const urgentTask = document.getElementById("urgent-task");
+const date = document.getElementById("due-date");
+const isUrgentTask = document.getElementById('urgent-not-urgent');
+
+function showNewTodo({ id, text, isDone, isUrgent, dueDate, isFiltered }) {
+
+  const li = document.createElement("li");
+  li.className = "todo-item";
+  li.id = `item_${id}`;
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = isDone;
+
+  let taskText;
+
+  if (isUrgent) {
+    taskText = document.createTextNode(` ${text}. Task is urgent. Due date - ${dueDate}  `);
+  } else {
+    taskText = document.createTextNode(` ${text} `);
+  }
+  
+  const deleteButton = document.createElement("a");
+  deleteButton.className = "delete";
+  deleteButton.href = "#"
+  deleteButton.appendChild(document.createTextNode("x"));
+
+  li.appendChild(checkbox);
+  li.appendChild(taskText);
+  li.appendChild(deleteButton);
+
+  tasks.appendChild(li);
+
+  if (!isFiltered) {
+      li.style.display = "";
+    } else {
+      li.style.display = "none";
+    }
+
+}
+
+async function onNewTodo(event) {
+  event.preventDefault();
+
+  const text = taskInput.value;
+  if (text == '') return;
+  taskInput.value = '';
+
+  let isUrgent;
+  let dueDate;
+  let todo;
+
+  if (urgentTask.checked) {
+    isUrgent = true;
+  } else {isUrgent = false; }
+
+  if (isUrgent) {
+    
+    dueDate = date.value;
+    let response = await post('/api/urgenttask', { text, isDone: false, isUrgent: isUrgent, dueDate });
+    todo = await response.json();
+    urgentTask.checked = false;
+        
+    } else { 
+    
+        let response = await post('/api/task', { text, isDone: false, isUrgent: isUrgent });
+        todo = await response.json();
+  }
+  
+  showNewTodo(todo);
+
+}
+
+async function onStatusChanged(event) {
+  const target = event.target;
+
+  const checked = target.checked;
+  const parent = target.parentElement;
+  const [, id] = parent.id.split('_');
+
+    try {
+
+        let result = await post(`/api/task/${id}/update`, { isDone: checked }); 
+            if (!result.ok) {
+                target.checked = !checked;
+            }
+    } catch(ex) {
+        target.checked = !checked;
+    }
+
+}
+
+form.addEventListener("submit", onNewTodo);
+
+let existingTasks = await fetch('/api/tasks')
+    .then(res => res.json());
+
+existingTasks.forEach(t => showNewTodo(t));
+
+async function onTaskDelete (event) {
+
+  event.preventDefault();
+
+  const [, id] = event.target.parentNode.id.split('_');
+  
+  let isToDel = await post('/api/delete-task', {id: Number(id)});
+
+    if (!isToDel.ok) {
+        return;
+    } else {
+        event.target.parentNode.remove();
+    }
+  
+}
+
+function onTasks (event) {
+  const target = event.target;
+
+  if (target.nodeName === "INPUT") {
+    onStatusChanged(event);
+  } else if (target.nodeName === "A" & target.classList.contains('delete')) {
+    onTaskDelete(event);
+  } else { 
+    return;
+    }
+}
+
+tasks.addEventListener("click", onTasks);
+
+async function onClearTasks(event) {
+  event.preventDefault();
+
+  const allTasks = Array.from(tasks.children);
+
+  let isRemovedTasks = await post('api/clear', allTasks);
+   
+    if (!isRemovedTasks.ok) {
+        return;
+    } else {
+        allTasks.forEach(task => task.remove());
+    }
+  
+}
+
+clearTasksButton.addEventListener('click', onClearTasks);
+
+const filter = document.getElementById('filter');
+
+async function onFilter(event) {
+event.preventDefault();
+
+const tasksForFiltering = document.querySelectorAll("li.todo-item");
+
+const text = filter.value;
+let isFiltered = await post('api/filter', {text: text});
+
+    if (!isFiltered.ok) {
+        return;
+
+    } else {
+    
+        tasksForFiltering.forEach(task => {
+            if (task.textContent.includes(text)) {
+            task.style.display = "";
+            } else {
+            task.style.display = "none";
+            }
+        });
+    }
+  filter.value = "";
+  
+}
+
+filter.addEventListener('keyup', onFilter);
--- server.js
+++ server.js
@@ -9,6 +9,53 @@ const app = express();
 // settings
 const port = 3000;
 
+class Tasks {
+  _isDone = false;
+  _isUrgent = false;
+
+  constructor(id, text, isDone, isUrgent) {
+    this._isDone = isDone || this._isDone;
+    this._isUrgent = isUrgent || this._isUrgent;
+    this._id = id;
+    this._text = text;
+  }
+
+  get isDone() {
+    return this._isDone;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get text() {
+    return this._text;
+  }
+
+  get isUrgent() {
+    return this._isUrgent;
+  }
+
+}
+
+class UrgentTasks extends Tasks {
+  _isUrgent = true;
+
+  constructor(id, text, isDone, isUrgent, dueDate) {
+    super(id, text, isDone);
+    this._dueDate = dueDate;
+    this._isUrgent = isUrgent || this._isUrgent;
+  }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+
+  get isUrgent(){
+    return this._isUrgent;
+  }
+}
+
 const state = {
   count: 0,
   items: [],
@@ -33,11 +80,21 @@ let taskList = state.tasks;
 let id = 1;
 
 app.post('/api/task', (req, res) => {
-  const task = { id, text: req.body.text, isDone: req.body.isDone };
-  state.tasks.push(task);
+  const task = { id, text: req.body.text, isDone: req.body.isDone, isUrgent: req.body.isUrgent };
+  state.tasks.push(new Tasks(task.id, task.text, task.isDone, task.isUrgent));
   id += 1;
   res.json(task);
 });
+
+app.post('/api/urgenttask', (req, res) => {
+  const task = { id, text: req.body.text, isDone: req.body.isDone, dueDate: req.body.dueDate, isUrgent: req.body.isUrgent };
+  
+  state.tasks.push(new UrgentTasks(task.id, task.text, task.isDone, task.isUrgent, task.dueDate));
+  id += 1;
+  res.json(task);
+  
+});
+
 
 app.post('/api/task/:id/update', (req, res) => {
   const id = Number(req.params.id);
@@ -54,7 +111,6 @@ app.post('/api/task/:id/update', (req, res) => {
 
 app.post('/api/clear', (req, res) => {
   state.tasks = [];
-  console.log(state.tasks);
   res.end();
 })
 
